335a4f36e0664b92a5a98525d83a14df5707920e
LefTonbo <dragonbird.tonbo@gmail.com>
2017-04-22 04:14:01 +0900

    First commit

---
+++ conf/default.php
@@ -0,0 +1,8 @@
+<?php
+/**
+ * Default settings for the avoidsearchbot plugin
+ *
+ * @author LefTobno <dragonbird.tonbo@gmail.com>
+ */
+
+//$conf['fixme']    = 'FIXME';
---
+++ conf/metadata.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Options for the avoidsearchbot plugin
+ *
+ * @author LefTobno <dragonbird.tonbo@gmail.com>
+ */
+
+
+//$meta['fixme'] = array('string');
+
